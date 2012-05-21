308e3c018f45361e16597299af5d6eb938d39d6d
detule <ogjoneski@gmail.com>
2012-05-21 10:01:24 -0400

    libaudio_wince: Fix Android.mk (from Cyanogen)

--- libaudio_wince/Android.mk
+++ libaudio_wince/Android.mk
@@ -1,3 +1,4 @@
+
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),rhodium)
 ifneq ($(BUILD_TINY_ANDROID),true)
 
@@ -5,21 +6,18 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := audio_policy.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_MODULE:= audio_policy.$(TARGET_BOOTLOADER_BOARD_NAME)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_STATIC_LIBRARIES := libmedia_helper
+LOCAL_WHOLE_STATIC_LIBRARIES := libaudiopolicy_legacy
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := \
-    libmedia_helper \
-    libaudiopolicy_legacy
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libutils \
     libmedia
 
-LOCAL_SRC_FILES:=               \
-    AudioPolicyManager.cpp
-
+LOCAL_SRC_FILES:= AudioPolicyManager.cpp
 
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
   LOCAL_CFLAGS += -DWITH_A2DP
@@ -27,35 +25,29 @@ endif
 
 include $(BUILD_SHARED_LIBRARY)
 
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := audio.primary.$(TARGET_BOOTLOADER_BOARD_NAME)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_STATIC_LIBRARIES += libmedia_helper
+LOCAL_WHOLE_STATIC_LIBRARIES := libaudiohw_legacy
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := \
-    libmedia_helper \
-    libaudiohw_legacy
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libutils \
     libmedia \
-    libhardware_legacy
-ifneq ($(TARGET_SIMULATOR),true)
-    LOCAL_SHARED_LIBRARIES += libdl
-endif
+    libhardware_legacy \
+    libdl
 
 LOCAL_SRC_FILES += AudioHardware.cpp
 
-ifeq ($TARGET_OS)-$(TARGET_SIMULATOR),linux-true)
-    LOCAL_LDLIBS += -ldl
-endif
-
 LOCAL_CFLAGS += -fno-short-enums
 
-ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-  LOCAL_SHARED_LIBRARIES += audio.a2dp.default libbinder
-endif
+#ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+#  LOCAL_SHARED_LIBRARIES += audio.a2dp.default
+#  #endif
 
 include $(BUILD_SHARED_LIBRARY)
 
